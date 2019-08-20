0937d7a8073a7dff4bb594a970ae9979c5020018
root <root@ip-172-31-41-197.us-east-2.compute.internal>
2019-08-20 12:35:19 +0000

    edited two

--- hello1.java
+++ hello1.java
@@ -5,7 +5,7 @@ class hello1
 { 
     public static void main(String args[]) 
     { 
-        for(int i=0;i<5;i++)
+        for(int i=0;i<10;i++)
         System.out.println("Hello, World"); 
     } 
 } 
